d1e58cbbfe79b252eb833efce90678c1af558233
Guntars Asmanis-Graham <gunchars@gmail.com>
2016-06-22 16:54:15 -0400

    Initial commit

---
+++ custom.d.ts
@@ -0,0 +1,5 @@
+declare module "react-router" {
+  interface IRouteProps {
+    query: any
+  }
+}
---
+++ index.tsx
@@ -0,0 +1,7 @@
+/// <reference path="typings/index.d.ts"/>
+/// <reference path="custom.d.ts"/>
+
+import * as React from "react"
+import {Route} from "react-router"
+
+let route = <Route path="/" query={ "hellooo" } />
